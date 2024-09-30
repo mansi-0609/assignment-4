49459484fc592ee4e67199a8afe5e466186d5855
mansi-0609 <prachisoni0609@gmail.com>
2024-09-30 17:07:41 +0530

    Update ptr35.java

---
+++ ptr35.java
@@ -0,0 +1,15 @@
+class ptr35{
+    public static void main(String[] args) {  
+       for(int i=1;i<=5;i++){
+       
+        for(int j=5;j>=1;j--){
+          if(i==1||j==5||i==j)
+            System.out.print("*");         
+          else
+            System.out.print(" ");
+        }System.out.println();
+         
+       }
+    }
+}
+
